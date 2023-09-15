887936912df5b35c0ebe080fe364cd2e98982921
ahadbek1982 <aqpowerbi@gmail.com>
2023-09-15 17:42:32 +0500

    login settings

--- customers/settings.py
+++ customers/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework_simplejwt',
     "corsheaders",
     'rest_framework',
     'customers',
@@ -52,6 +53,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
+
+
 CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:3000",
--- customers/views.py
+++ customers/views.py
@@ -1,12 +1,14 @@
 from customers.models import Customers
 from customers.serializers import CustomersSerializers
 from django.http import JsonResponse, Http404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def customers(request):
     if request.method == 'GET':
         data = Customers.objects.all()
@@ -20,6 +22,7 @@ def customers(request):
 
 
 @api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def customer(request, id):
     try:
 
